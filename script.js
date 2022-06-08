2446c464ed2087fafba3ac1b9d780ea21e6b915d
zDarkOrdening <zdarkordening@gmail.com>
2022-06-07 22:34:52 -0300

    add floor and tryAgain event

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-const mario = document.querySelector(".mario")
-const pipe = document.querySelector(".pipe")
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
+const score = document.querySelector(".score");
 
 const jump = () => {
-    mario.classList.add('jump');
+  mario.classList.add("jump");
 
-    setTimeout(() => {
-        mario.classList.remove('jump');
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
 
-    }, 500);
+const tryAgain = () => {
+  document.location.reload(true)
 }
 
 const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+  const cloudsPosition = clouds.offsetLeft;
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition <= 80) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition <= 80) {
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${(pipePosition)}px`;
+    mario.src = "./mariojump_images/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "45px";
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${(marioPosition)}px`;
+    clouds.style.animation = "none";
+    clouds.style.left = `${cloudsPosition}px`;
 
-        mario.src = './mariojump_images/game-over.png';
-        mario.style.width = '75px'
-        mario.style.marginLeft = '45px'
+    clearInterval(loop);
 
-        clearInterval(loop);
-        
-    }
+    let gameOver = document.querySelector(".gameOver");
+    gameOver.innerText = "You lose!\n Press any key to try again";
 
-}, 10);
+    setTimeout(() => {
+      document.addEventListener("keydown", tryAgain);
+    }, 500)
+  }
+}, 10)
 
-document.addEventListener('keydown', jump);
+document.addEventListener("keydown", jump);
